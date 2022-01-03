d14690dad81610eb830da7345cf831c69a5627f8
Sebastian Czyrny <85814475+Sebastian-Czyrny@users.noreply.github.com>
2022-01-02 20:37:19 -0500

    Update ELImage.py

--- ELImage.py
+++ ELImage.py
@@ -1,32 +1,70 @@
+from PIL import Image
+from PIL import ImageDraw
+
+white = (255)
+black = (0)
+
 # contains relevant EL image information
 class ELImg:
+
     def __init__(self, image, id, relevantPixelDelimiter=100, extremaDelimiter=300):
         self.id = id
+        self.relevantPixelDelimiter = relevantPixelDelimiter
         self.pixelList = list(image.getdata())
+
+        # number of pixels with summed values greater than the relevantPixelDelimiter
         self.relevantPixelCount = 0
+
+        # total pixels in image
         self.totalPixelCount = 0
-        self.extremaPixelCount = 0      # number of pixels if summed values greater than the extremaDelimiter
-        self.relevantTotalRatio = 0.0   # % of image that is relevant
-        self.extremaRelevantRatio = 0.0 # % of relevant pixels that are extrema pixels
-        self.extremaTotalRatio = 0.0    # % of image with intense luminosity as defined by the extrema delimiter
-        self.pixelMax = 0               # highest summed up pixel value in whole image
-        self.extremaMode = dict()       # key is the summed up pixel value, value is the the number of that pixel value's occurrences in image
-        self.intensityRating = 0.0      # summed up weighting of all pixel values above the relevantPixelDelimiter threshold
-        self.intensityRelevantRatio = 0.0   # the 'true' luminosity of the cell, the area of the solar cell represented in picture does not matter
+
+        # number of pixels with summed values greater than the extremaDelimiter
+        self.extremaPixelCount = 0      
+
+        # % of image that is relevant
+        self.relevantTotalRatio = 0.0   
+
+        # % of relevant pixels that are extrema pixels
+        self.extremaRelevantRatio = 0.0 
+
+        # % of image with intense luminosity as defined by the extrema delimiter
+        self.extremaTotalRatio = 0.0    
+
+        # highest summed up pixel value in whole image
+        self.pixelMax = 0               
+
+        # key is the summed up pixel value, value is the the number of that pixel value's occurrences in image
+        self.extremaMode = dict()       
+
+        # summed up weighting of all pixel values above the relevantPixelDelimiter threshold
+        self.intensityRating = 0.0      
+
+        # the 'true' luminosity of the cell, the area of the solar cell represented in picture does not matter
+        self.intensityRelevantRatio = 0.0   
         
+        
+        # calculate totalPixelCount to be the number of pixels in the image in total
+        self.width, self.height = image.size
+        self.totalPixelCount = self.width * self.height 
+
 
         # find the largest pixel value
         for colour in image.getextrema():
             self.pixelMax = self.pixelMax + colour[1]
-
+        
         # Initialize relevantPixelCount to be the number of pixels where the R + G + B value is greater than 100
         # Find the extrema, and the number of occurences
+
         for pixel in self.pixelList:
             currPixel = pixel[0] + pixel[1] + pixel[2]
+
+            # evaluate the relevant pixels
             if currPixel > relevantPixelDelimiter:   
                 self.relevantPixelCount = self.relevantPixelCount + 1
                 self.intensityRating = self.intensityRating + 5*currPixel/3000  - 5*relevantPixelDelimiter/3000
 
+
+            # evaluate the extrema pixels
             if currPixel >= extremaDelimiter: 
                 self.extremaPixelCount = self.extremaPixelCount + 1
                 if currPixel in self.extremaMode.keys():
@@ -36,10 +74,7 @@ class ELImg:
         
         self.intensityRating = round(self.intensityRating, 3)
         
-        # calculate totalPixelCount to be the number of pixels in the image in total
-        self.width, self.height = image.size
-        self.totalPixelCount = self.width * self.height 
-
+        
         # calculate pixel count ratios (to 3 decimal places)
         self.relevantTotalRatio = round(self.relevantPixelCount / self.totalPixelCount * 100, 3)
         if self.relevantPixelCount == 0:
@@ -50,7 +85,42 @@ class ELImg:
         if self.relevantPixelCount == 0:
                 self.intensityRelevantRatio = 0.0
         else:
-            self.intensityRelevantRatio = round(self.intensityRating / self.relevantPixelCount * 100, 3)
+            self.intensityRelevantRatio = round(self.intensityRating / self.relevantPixelCount * 100, 3)    
+
+
+    # creates a projection of the relevant pixels of the image
+    def ProjImage(self, fp, weighted=False):
+        size = (self.width, self.height)
+        projectedImage = Image.new('L', size, color=black)
+        draw = ImageDraw.Draw(projectedImage, mode='L')
+        if not weighted:
+            currentPosition = [0,0]
+            for pixel in self.pixelList:
+                currPixel = pixel[0] + pixel[1] + pixel [2]
+                if currPixel > self.relevantPixelDelimiter:
+                    draw.point(currentPosition, fill=white)
+
+                # increment position by 1
+                if currentPosition[0] < self.width - 1:
+                    currentPosition[0] = currentPosition[0] + 1
+                else:
+                    currentPosition[0] = 0
+                    currentPosition[1] = currentPosition[1] + 1
+        else:
+            currentPosition = [0,0]
+            for pixel in self.pixelList:
+                currPixel = pixel[0] + pixel[1] + pixel [2]
+                if currPixel > self.relevantPixelDelimiter:
+                    draw.point(currentPosition, fill=currPixel)
+
+                # increment position by 1
+                if currentPosition[0] < self.width - 1:
+                    currentPosition[0] = currentPosition[0] + 1
+                else:
+                    currentPosition[0] = 0
+                    currentPosition[1] = currentPosition[1] + 1
+        projectedImage.save(fp)
+
 
 
     # print out contents of ELimage
